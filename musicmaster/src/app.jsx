d32f592ce0dadb0b0458644169b4e6734da99e7a
Tony <saric.tony@gmail.com>
2017-07-14 14:53:34 -0700

    adds spotify api but needs key, will continue later after i get key

--- musicmaster/src/app.jsx
+++ musicmaster/src/app.jsx
@@ -3,14 +3,37 @@ import './app.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search(){
+    const BASE_URL = 'https://musicbrainz.org/ws/2/artist?';
+    const FETCH_URL = `${BASE_URL}query=${this.state.query}&type=artist&limit=1`;
+    console.log(FETCH_URL);
+  }
+
+
   render(){
     return(
       <div className="App">
         <div className="App-title">Music Master</div>
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder = "Search for an Artist"/>
-            <InputGroup.Addon>
+            <FormControl  type="text" 
+                          placeholder = "Search for an Artist" 
+                          value={this.state.query}
+                          onChange={event =>{this.setState({query: event.target.value})}}
+                          onKeyPress={event => {
+                            if (event.key === 'Enter'){
+                              this.search()
+                            }
+                          }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
